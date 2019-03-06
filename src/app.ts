204d2bc657918866295ea57681b3aea6ca485bfa
Porawit Poboonma <ball6847@gmail.com>
2019-03-06 12:03:05 +0700

    add poc

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('Hello World')
+import { Storage } from './lib/session-storage-sync/Storage';
+
+const storage = new Storage();
+const btn = document.getElementById('btn') as HTMLButtonElement;
+const input = document.getElementById('input') as HTMLInputElement;
+const placeholder = document.getElementById('value-placeholder');
+
+function renderStorageValue() {
+  const value = storage.session.get('value');
+  console.log(Date.now(), 'rendering', value);
+  placeholder.innerHTML = value;
+}
+
+btn.addEventListener('click', () => {
+  const value = input.value;
+  storage.session.set('value', value);
+  renderStorageValue();
+});
+
+window.addEventListener('load', () => {
+  // it takes some time before it's ready
+  setTimeout(() => renderStorageValue(), 100);
+});
